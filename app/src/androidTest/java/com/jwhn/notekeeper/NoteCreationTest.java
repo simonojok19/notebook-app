47d2aeb693ae120f5ce2ffc122d1eeb8f08b2d7d
Simon Peter Ojok <simonojok19@gmail.com>
2020-01-14 09:15:58 +0300

    working on the project started

---
+++ app/src/androidTest/java/com/jwhn/notekeeper/NoteCreationTest.java
@@ -0,0 +1,45 @@
+package com.jwhn.notekeeper;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class NoteCreationTest {
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetUp() {
+        sDataManager = DataManager.getInstance();
+    }
+
+    @Rule
+    public ActivityTestRule<NoteListActivity> mNoteListActivityRule =
+            new ActivityTestRule<>(NoteListActivity.class);
+
+    @Test
+    public void createNewNote() {
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body of our test note";
+
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText),
+                closeSoftKeyboard());
+//        onView(withId(R.id.text_note_text)).check(matches)
+        pressBack();
+    }
+
+}
---
+++ app/src/androidTest/java/com/jwhn/notekeeper/NoteListActivityTestpo.java
@@ -0,0 +1,192 @@
+package com.jwhn.notekeeper;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class NoteListActivityTestpo {
+
+    @Rule
+    public ActivityTestRule<NoteListActivity> mActivityTestRule = new ActivityTestRule<>(NoteListActivity.class);
+
+    @Test
+    public void noteListActivityTestpo() {
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.text_note_title),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatSpinner = onView(
+                allOf(withId(R.id.spinner_courses),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        appCompatSpinner.perform(click());
+
+        ViewInteraction appCompatSpinner2 = onView(
+                allOf(withId(R.id.spinner_courses),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                0),
+                        isDisplayed()));
+        appCompatSpinner2.perform(click());
+
+        DataInteraction appCompatCheckedTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(1);
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.text_note_title),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatEditText2.perform(click());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.text_note_title),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("a"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.text_note_title), withText("a"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatEditText4.perform(click());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.text_note_title), withText("a"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatEditText5.perform(replaceText("Tt"));
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.text_note_title), withText("Tt"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                1),
+                        isDisplayed()));
+        appCompatEditText6.perform(closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.text_note_text),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatEditText7.perform(replaceText("vssn"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.text_note_text), withText("vssn"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatEditText8.perform(replaceText("vssnvgc"));
+
+        ViewInteraction appCompatEditText9 = onView(
+                allOf(withId(R.id.text_note_text), withText("vssnvgc"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                        1),
+                                2),
+                        isDisplayed()));
+        appCompatEditText9.perform(closeSoftKeyboard());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
